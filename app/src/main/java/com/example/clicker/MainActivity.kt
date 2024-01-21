7a292679c005dea99cc5a0540a21a994bf36c30a
RJ Fortune <rfor9973@students.vsu.edu>
2024-01-21 17:04:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/clicker/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.clicker
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    var counter = 0
+
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<ImageButton>(R.id.PlusButton)
+
+        val textView = findViewById<TextView>(R.id.CCounter)
+
+        val upgradeButton = findViewById<Button>(R.id.UpgradeBtn)
+
+        button.setOnClickListener {
+            //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
+
+            if (counter >= 100) {
+                // Show upgrade button and set onClickListener
+                upgradeButton.visibility = View.VISIBLE
+                upgradeButton.setOnClickListener {
+
+                    // Update original button to add 2 instead of `
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+
+                    // Hide upgrade button again
+                    upgradeButton.visibility = View.INVISIBLE
+
+                }
+            }
+        }
+    }
+}
